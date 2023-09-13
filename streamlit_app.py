60d59bdc486b6b62393137c8ec15507a9cd7e3dd
gborasio <gborasio@gmail.com>
2023-09-13 20:05:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 ')
+streamlit.text('Kaler, Spinach')
+streamlit.text('Hard VBoiled')
